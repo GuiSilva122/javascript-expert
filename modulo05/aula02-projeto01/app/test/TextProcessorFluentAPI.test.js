a399d201961b97d8b7f81c34da90bbc343e91a03
Guilherme de Souza Silva <gui.iug.net@gmail.com>
2022-07-12 14:01:37 -0300

    Regex

--- modulo05/aula02-projeto01/app/test/TextProcessorFluentAPI.test.js
+++ modulo05/aula02-projeto01/app/test/TextProcessorFluentAPI.test.js
@@ -1,6 +1,6 @@
 const { describe, it } = require("mocha");
 const { expect } = require("chai");
-const TextProcessorFluentAPI = require("./../src/textProcessorFluentAPI");
+const TextProcessorFluentAPI = require("../src/textProcessorFluentAPI");
 const mock = require("./mock/valid");
 
 describe("TextProcessorAPI", () => {
